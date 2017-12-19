34ba6f79088c4199bb5a0a5f4a7a77be60d895e1
Alex Jones <alexander.jones@softwire.com>
2017-12-19 23:21:16 +0000

    Add joystick axis helper methods

--- src/input/joystick.rs
+++ src/input/joystick.rs
@@ -57,24 +57,67 @@ impl JoystickInput {
     pub fn get_button_10(&self) -> bool {
         self.devices.iter().fold(false, |acc, device| {acc || device.button_10})
     }
+
     pub fn get_x_axis(&self) -> f64 {
-        self.devices.iter().fold(0.0, |acc, device| {if device.x_axis.abs() > acc.abs() {device.x_axis} else {0.0}})
+        self.devices.iter().fold(0.0, |acc, device| {if device.x_axis.abs() > acc.abs() {device.x_axis} else {acc}})
     }
+    pub fn get_x_axis_left_flag(&self, deadzone: f64) -> bool {
+        self.get_x_axis() < -deadzone
+    }
+    pub fn get_x_axis_right_flag(&self, deadzone: f64) -> bool {
+        self.get_x_axis() > deadzone
+    }
+
     pub fn get_y_axis(&self) -> f64 {
-        self.devices.iter().fold(0.0, |acc, device| {if device.y_axis.abs() > acc.abs() {device.y_axis} else {0.0}})
+        self.devices.iter().fold(0.0, |acc, device| {if device.y_axis.abs() > acc.abs() {device.y_axis} else {acc}})
     }
+    pub fn get_y_axis_left_flag(&self, deadzone: f64) -> bool {
+        self.get_y_axis() < -deadzone
+    }
+    pub fn get_y_axis_right_flag(&self, deadzone: f64) -> bool {
+        self.get_y_axis() > deadzone
+    }
+
     pub fn get_z_axis(&self) -> f64 {
-        self.devices.iter().fold(0.0, |acc, device| {if device.z_axis.abs() > acc.abs() {device.z_axis} else {0.0}})
+        self.devices.iter().fold(0.0, |acc, device| {if device.z_axis.abs() > acc.abs() {device.z_axis} else {acc}})
     }
+    pub fn get_z_axis_left_flag(&self, deadzone: f64) -> bool {
+        self.get_z_axis() < -deadzone
+    }
+    pub fn get_z_axis_right_flag(&self, deadzone: f64) -> bool {
+        self.get_z_axis() > deadzone
+    }
+
     pub fn get_rx_axis(&self) -> f64 {
-        self.devices.iter().fold(0.0, |acc, device| {if device.rx_axis.abs() > acc.abs() {device.rx_axis} else {0.0}})
+        self.devices.iter().fold(0.0, |acc, device| {if device.rx_axis.abs() > acc.abs() {device.rx_axis} else {acc}})
     }
+    pub fn get_rx_axis_left_flag(&self, deadzone: f64) -> bool {
+        self.get_rx_axis() < -deadzone
+    }
+    pub fn get_rx_axis_right_flag(&self, deadzone: f64) -> bool {
+        self.get_rx_axis() > deadzone
+    }
+
     pub fn get_ry_axis(&self) -> f64 {
-        self.devices.iter().fold(0.0, |acc, device| {if device.ry_axis.abs() > acc.abs() {device.ry_axis} else {0.0}})
+        self.devices.iter().fold(0.0, |acc, device| {if device.ry_axis.abs() > acc.abs() {device.ry_axis} else {acc}})
     }
+    pub fn get_ry_axis_left_flag(&self, deadzone: f64) -> bool {
+        self.get_ry_axis() < -deadzone
+    }
+    pub fn get_ry_axis_right_flag(&self, deadzone: f64) -> bool {
+        self.get_ry_axis() > deadzone
+    }
+
     pub fn get_rz_axis(&self) -> f64 {
-        self.devices.iter().fold(0.0, |acc, device| {if device.rz_axis.abs() > acc.abs() {device.rz_axis} else {0.0}})
+        self.devices.iter().fold(0.0, |acc, device| {if device.rz_axis.abs() > acc.abs() {device.rz_axis} else {acc}})
     }
+    pub fn get_rz_axis_left_flag(&self, deadzone: f64) -> bool {
+        self.get_rz_axis() < -deadzone
+    }
+    pub fn get_rz_axis_right_flag(&self, deadzone: f64) -> bool {
+        self.get_rz_axis() > deadzone
+    }
+
     pub fn get_hat_switch(&self) -> HatSwitch {
         self.devices.iter().fold(HatSwitch::Center, |acc, device| {if device.hat_switch != HatSwitch::Center {device.hat_switch} else {acc}})
     }
