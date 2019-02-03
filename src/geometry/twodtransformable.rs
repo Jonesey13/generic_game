e82f9b9ccedbcc7659e1b3c2487d146b803ab296
Alex Jones <alexander.jones@softwire.com>
2019-02-03 11:23:52 +0000

    Force auto-impl out to games

--- src/geometry/twodtransformable.rs
+++ src/geometry/twodtransformable.rs
@@ -13,23 +13,3 @@ pub trait HasTwoDTransformable {
     fn get_transformable(&self) -> &Self::Transformable;
     fn get_transformable_mut(&mut self) -> &mut Self::Transformable;
 }
-
-impl<T> TwoDTransformable for HasTwoDTransformable<Transformable=T>
-    where T : TwoDTransformable
-{
-    fn shift_by(&mut self, shift: Point) {
-        self.get_transformable_mut().shift_by(shift);
-    }
-
-    fn rotate(&mut self, rot_angle: f64) {
-        self.get_transformable_mut().rotate(rot_angle);
-    }
-
-    fn get_center(&self) -> Point {
-        self.get_transformable().get_center()
-    }
-
-    fn scale_by(&mut self, scale_factor: f64) {
-        self.get_transformable_mut().scale_by(scale_factor);
-    }
-}
