4fd2681de7f4e2df1cf3e9f8dc35b3234c3a1131
Alex Jones <alexander.jones@softwire.com>
2019-09-08 17:46:42 +0100

    Add cartesian rectangle contains method

--- src/geometry/cartesian_rectangle.rs
+++ src/geometry/cartesian_rectangle.rs
@@ -14,4 +14,14 @@ impl CartesianRectangle {
     pub fn y_bounds(&self) -> Point {
         Point::new(self.center.y - self.height / 2.0, self.center.y + self.height / 2.0)
     }
+
+    pub fn contains_point(&self, point: Point) -> bool {
+        let x_bounds = self.x_bounds();
+        let y_bounds = self.y_bounds();
+
+        point.x < x_bounds.y 
+        && point.x > x_bounds.x
+        && point.y < y_bounds.y
+        && point.y > y_bounds.x
+    }
 }
